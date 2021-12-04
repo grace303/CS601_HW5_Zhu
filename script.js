aab9fadd0ee3450a5196390622bfab2d698f911e
grace303 <yanruz@bu.edu>
2021-12-04 14:25:44 -0800

    add in status check

--- script.js
+++ script.js
@@ -15,42 +15,47 @@ document.addEventListener("DOMContentLoaded", function (event) {
         div.innerHTML = "";
 
         fetch('https://grace303.github.io/CS601_HW5_Zhu/degrees.json')
-            .then(response => response.json())
-            .then(data => {
-                //1. Create a table
-                const table = document.createElement('table');
+            .then(response => {
+                if (response.status == 200) {
+                    response.json().then(data => {
+                        //1. Create a table
+                        const table = document.createElement('table');
 
-                //2. Get table header, like "school", "major" etc.
-                let headers = [];
-                for (let item in data.my_degrees[0]) {
-                    headers.push(item);
+                        //2. Get table header, like "school", "major" etc.
+                        let headers = [];
+                        for (let item in data.my_degrees[0]) {
+                            headers.push(item);
+                        }
+
+                        //3. Create table header with "school", "major" etc.
+                        let tr = table.insertRow(-1);
+                        for (let i = 0; i < headers.length; i++) {
+                            let th = document.createElement("th");
+                            th.innerHTML = headers[i];
+                            tr.appendChild(th);
+                        }
+
+                        //4. Create other table rows with detailed information
+                        for (let i = 0; i < data.my_degrees.length; i++) {
+                            let tr = table.insertRow(-1);
+                            let degree = data.my_degrees[i];
+                            for (let header of headers) {
+                                let td = document.createElement("td");
+                                td.innerHTML = degree[header];
+                                tr.appendChild(td);
+                            }
+
+                        }
+
+                        //5. Add the whole table to the page
+                        div.appendChild(table);
+
+                    });
                 }
-
-                //3. Create table header with "school", "major" etc.
-                let tr = table.insertRow(-1);
-                for (let i = 0; i < headers.length; i++) {
-                    let th = document.createElement("th");
-                    th.innerHTML = headers[i];
-                    tr.appendChild(th);
+                else {
+                    div.innerHTML = "Sorry, there's an error!";
                 }
-
-                //4. Create other table rows with detailed information
-                for (let i = 0; i < data.my_degrees.length; i++) {
-                    let tr = table.insertRow(-1);
-                    let degree = data.my_degrees[i];
-                    for (let header of headers) {
-                        let td = document.createElement("td");
-                        td.innerHTML = degree[header];
-                        tr.appendChild(td);
-                    }
-
-                }
-
-                //5. Add the whole table to the page
-                div.appendChild(table);
-
             });
-
 
     });
 
