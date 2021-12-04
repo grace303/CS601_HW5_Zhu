e6456731c388f22f45bdd420381f2af745e90da2
grace303 <yanruz@bu.edu>
2021-12-04 11:44:01 -0800

    use add event listener

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
 
 /*jshint esversion: 6 */
 /*jshint esversion: 8 */
-async function getDegrees(url) {
-    // fetch the url
-    await fetch('https://grace303.github.io/CS601_HW5_Zhu/degrees.json')
-        //get your data here, and check for the response status. If it's not 200, throw an error
-        .then((response) => response.json())
-        .then((data) => console.log(data)
-        );
-}
 
-getDegrees("./src/degrees.json");
+document.addEventListener("DOMContentLoaded", function (event) {
+    fetch('https://grace303.github.io/CS601_HW5_Zhu/degrees.json')
+        .then(response => response.json())
+        .then(data => {
+            console.log('My data', data);
+        });
+
+});
