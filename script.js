005547605fbefeef8976e39d99899aa1f8cee1df
grace303 <yanruz@bu.edu>
2021-12-04 13:22:43 -0800

    add in button

--- script.js
+++ script.js
@@ -2,52 +2,61 @@
 /*jshint esversion: 6 */
 /*jshint esversion: 8 */
 
+//Do everything when the doc is ready
 document.addEventListener("DOMContentLoaded", function (event) {
-    fetch('https://grace303.github.io/CS601_HW5_Zhu/degrees.json')
-        .then(response => response.json())
-        .then(data => {
-            //1. Create a table
-            const table = document.createElement('table');
 
-            //2. Get table header, like "school", "major" etc.
-            let headers = [];
-            for (let item in data.my_degrees[0]) {
-                headers.push(item);
-            }
+    let button = document.getElementById('btn');
 
-            //3. Create table header with "school", "major" etc.
-            let tr = table.insertRow(-1);
-            for (let i = 0; i < headers.length; i++) {
-                let th = document.createElement("th");
-                th.innerHTML = headers[i];
-                tr.appendChild(th);
-            }
 
-            //4. Create other table rows with detailed information
-            for (let i = 0; i < data.my_degrees.length; i++) {
-                let tr = table.insertRow(-1);
-                let degree = data.my_degrees[i];
-                for (let header of headers) {
-                    let td = document.createElement("td");
-                    td.innerHTML = degree[header];
-                    tr.appendChild(td);
+    //Add event listener to the button when clicking
+    button.addEventListener("click", function (event) {
+        //Clear everything when clicking on the button
+        let div = document.getElementById('container');
+        div.innerHTML = "";
+
+        fetch('https://grace303.github.io/CS601_HW5_Zhu/degrees.json')
+            .then(response => response.json())
+            .then(data => {
+                //1. Create a table
+                const table = document.createElement('table');
+
+                //2. Get table header, like "school", "major" etc.
+                let headers = [];
+                for (let item in data.my_degrees[0]) {
+                    headers.push(item);
                 }
 
-            }
+                //3. Create table header with "school", "major" etc.
+                let tr = table.insertRow(-1);
+                for (let i = 0; i < headers.length; i++) {
+                    let th = document.createElement("th");
+                    th.innerHTML = headers[i];
+                    tr.appendChild(th);
+                }
 
-            //5. Add the whole table to the page
-            let div = document.getElementById('container');
-            div.appendChild(table);
+                //4. Create other table rows with detailed information
+                for (let i = 0; i < data.my_degrees.length; i++) {
+                    let tr = table.insertRow(-1);
+                    let degree = data.my_degrees[i];
+                    for (let header of headers) {
+                        let td = document.createElement("td");
+                        td.innerHTML = degree[header];
+                        tr.appendChild(td);
+                    }
 
-        });
+                }
+
+                //5. Add the whole table to the page
+                div.appendChild(table);
+
+            });
 
 
-
-
-
-
-
+    });
 
 
 
 });
+
+
+
